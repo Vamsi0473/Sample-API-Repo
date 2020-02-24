16368e19e6aadc43257f0315ef49d2027a0c32fc
Vikash Sharma <Vikash.Sharma@spglobal.com>
2020-02-24 13:59:09 +0530

    commit code

---
+++ SampleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [Route("marketdata")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly List<string> Cities = new List<string> { "Hyderabad", "Bangalore" };
+        [HttpGet("v1/list")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Cities.ToArray();
+        }
+
+        [HttpGet("v1/data")]
+        public ActionResult<string> Get(int id)
+        {
+            return Cities.ToArray()[id];
+        }
+    }
+}
